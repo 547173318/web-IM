356b463d01f024d4fcf4cd3bb3b5951a3b693960
ll1998 <547173318@qq.com>
2021-12-06 23:43:09 +0800

    web-IM

--- coding/listening-me/main/main.go
+++ coding/listening-me/main/main.go
@@ -2,9 +2,18 @@ package main
 
 import (
 	"fmt"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
 	"html/template"
 	"net/http"
 )
+// UserInfo 用户信息
+type UserInfo struct {
+	ID uint
+	Name string
+	Gender string
+	Hobby string
+}
 
 func sayHello(w http.ResponseWriter, r *http.Request) {
 	// 解析指定文件生成模板对象
@@ -17,8 +26,25 @@ func sayHello(w http.ResponseWriter, r *http.Request) {
 	tmpl.Execute(w, "沙河小王子")
 }
 func main() {
+	db, err := gorm.Open("mysql", "root:123456@(127.0.0.1:3306)/listening-me?charset=utf8mb4&parseTime=True&loc=Local")
+	if err!= nil{
+		panic(err)
+	}
+	defer db.Close()
+
+	// 自动迁移
+	db.AutoMigrate(&UserInfo{})
+
+	u1 := UserInfo{1, "七米", "男", "篮球"}
+	u2 := UserInfo{2, "沙河娜扎", "女", "足球"}
+	// 创建记录
+	db.Create(&u1)
+	db.Create(&u2)
+
+
+
 	http.HandleFunc("/", sayHello)
-	err := http.ListenAndServe(":9090", nil)
+	err = http.ListenAndServe(":9090", nil)
 	if err != nil {
 		fmt.Println("HTTP server failed,err:", err)
 		return
